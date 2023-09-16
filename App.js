e08ecd35ffa72ab22b423400a40a237bc87220cc
madisi-9398 <jayalaxmimadisi9398@gmail.com>
2023-09-17 00:40:11 +0530

    functional components

--- App.js
+++ App.js
@@ -13,22 +13,38 @@
  *
  */
 
+import React from "react"; //import indicates that the package is importing from node modules
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {},
-    "I am an h1 tag"),React.createElement("h2", {},
-    "I am an h2  tag")]
-  ), //Keep in an array for siblings
-); 
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2  tag"),
+  ]) //Keep in an array for siblings
+);
 
+const jsxHeading = <h1>Jsx h1 element</h1>;
+
+//React element
 const heading = React.createElement(
   "h1",
   { id: "heading" },
   "Hello World from React!"
 ); //obj is the attributes and React.createElement gives an js object
+
+const HeadingComp = () => {
+  //Functional compoents is a simple JS function which returns jsx
+  return (
+    <div>
+      {jsxHeading} {/* we can write any type of js code in the parenthesis */}
+      {100+200}
+      <h1>React Functional Component</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); //render method covert the heading object to a h1 tag and put it in the UI
+root.render(jsxHeading); //render method covert the heading object to a h1 tag and put it in the UI
+root.render(<HeadingComp />); // for componentd the syntax
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-//namste react
+# namste react
+# parcel
+- Dev build
+- host the app to the local server
+- HMR - Hot Module Rplaceement -- it autmatically refreshes the app when saved
+- File watching algorithm - written in c++ 
+- Catching - faster builds
+- Image optimization
+- Minification for production
+- buldles the files
+- compress the code
+- Consistent hashing
+- code silting
+- differential budling - support older browsers
+- Dignostics
+- error handling
+- https support
+- Tree shaking  - remove unused code for you
+- different dev and prod bundles
+
+# browsers list
+ - we can configure in which bowser our app should work
+
+# JSX
+- JS syntax which is used to create react elements
+
+- transpilling is doen by parcel
+means as jsx is not understood by js engine parcel convert the jsx to react/js engine undersytasndable
+- In jsx the attributes are given in camel case
+
+# babel
+JS compiler it transpiles jsx to code that js engine and react will understand
+
+- parcel has babel for transpiling
+
+
+# components
+- class based components
+- functional components
+
+# component composition
+- returning a component from another component
+  
